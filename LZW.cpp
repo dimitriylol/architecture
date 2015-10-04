1b513e6208c7705ba611c4fc05ce3aa0e1ff0a54
dimitriy <liman.dimitriy@gmail.com>
2015-10-04 17:55:40 +0300

    Whole new encode/decode

--- LZW.cpp
+++ LZW.cpp
@@ -7,144 +7,21 @@
 
 #include "LZW.hpp"
 
-void LZW::inc_bits_size(int val) {
-	try {
-		if (bits_size < get_bits_size(val))
-			bits_size++;
-	}
-	catch (const runtime_error& err) { }
+using namespace std;
+
+map<string, int> LZW::create_alphabet() {
+	dictSize = 256;
+	map<string, int> dictionary;
+	for (int i = 0; i < 256; i++)
+		dictionary[string(1, i)] = i;
+	return dictionary;
 }
 
-string LZW::dec2bin(int number) {
-    string result = "";
-    do {
-        if ( (number & 1) == 0 )
-            result += "0";
-        else
-            result += "1";
-        number >>= 1;
-    } while ( number );
-    result.resize(bits_size, '0');
-    reverse(result.begin(), result.end());
-    return result;
-}
-
-int bin2dec(string number) {
-    int result = 0, pow = 1;
-    for ( int i = number.length() - 1; i >= 0; --i, pow <<= 1 )
-        result += (number[i] - '0') * pow;
-    return result;
-}
-
-void LZW::add_to_alphabet(string str) {
-	inc_bits_size(value);
-	alphabet[str] = value++;
-}
-
-void LZW::add_to_reverse_alphabet(string str) {
-	inc_bits_size(value + 1);
-	reverse_alphabet[value++] = str;
-}
-
-string string_butlast_char(string str) {
-	string tmp = str.substr(0, str.length() - 1);
-	return (tmp.empty()) ? str : tmp;
-}
-
-int LZW::get_bits_size(int val) {
-	if (val == 0)
-		throw runtime_error("first ascii code == second ascii code");
-	return ceil(log10(val)/log10(2));
-}
-
-void LZW::create_alphabet() {
-	value = 0;
-	alphabet.clear();
-	char i = first_ascii;
-	while (i < last_ascii)
-		add_to_alphabet(string(1, i++));
-}
-
-void LZW::create_reverse_alphabet() {
-	create_alphabet();
-	char i = first_ascii;
-	value = 0;
-	while (i < last_ascii)
-		add_to_reverse_alphabet(string(1, i++));
-}
-
-LZW::LZW(char first_ascii, char last_ascii) {
-	if (first_ascii > last_ascii)
-		throw runtime_error("first ascii code larger than last ascii code");
-	LZW::first_ascii = first_ascii;
-	LZW::last_ascii = last_ascii;
-	bits_size = get_bits_size(last_ascii - first_ascii);
-	create_reverse_alphabet();
-}
-
-void LZW::print_alphabet() {
-	for (auto& kvp: alphabet)
-		cout << "key " << kvp.first << " val " << kvp.second << endl;
-	cout << "Val " << value << endl;
-}
-
-void LZW::print_rev_alphabet() {
-	for (auto& kvp: reverse_alphabet)
-		cout << "key " << kvp.first << " val " << kvp.second << endl;
-	cout << "Val " << value << endl;
-}
-
-
-string LZW::encode(string to_encode) {
-	create_alphabet();
-	print_alphabet();
-	string res;
-	bits_size = get_bits_size(last_ascii - first_ascii);
-	string::iterator next_sym = to_encode.begin();
-	string current_str = string(1, *next_sym++);
-	for (; next_sym != to_encode.end(); next_sym++) {
-		current_str += string(1, *next_sym);
-		cout << "cur str " << current_str;
-		if (alphabet.find(current_str) == alphabet.end()) {
-//			try {
-			cout << " res " << dec2bin(alphabet.at(string_butlast_char(current_str)));
-			cout << " str " << alphabet.at(string_butlast_char(current_str)) << endl;
-			res += dec2bin(alphabet.at(string_butlast_char(current_str)));
-//			}
-//			catch (out_of_range) { }
-			add_to_alphabet(current_str);
-			current_str = string(1, *next_sym);
-		}
-	}
-	//TODO: change cycle. Doesn't process last symbol
-	if (current_str.length() == 1)
-		res += dec2bin(alphabet.at(current_str));
-	print_alphabet();
-	return res;
-}
-
-string LZW::decode(string to_decode) {
-	create_reverse_alphabet();
-	string res;
-	bits_size = get_bits_size(last_ascii - first_ascii);
-	string current_str;
-	int num;
-	size_t i = 0;
-	while (i < to_decode.length()) {
-		num = bin2dec(to_decode.substr(i, bits_size));
-		i += bits_size;
-//		try {
-		current_str += reverse_alphabet.at(num);
-		if (alphabet.find(current_str) == alphabet.end()) {
-			current_str = current_str.substr(0, current_str.length() - reverse_alphabet.at(num).length() + 1);
-			add_to_reverse_alphabet(current_str);
-			current_str = reverse_alphabet.at(num);
-		}
-		res += reverse_alphabet.at(num);
-//		}
-//		catch (out_of_range) { }
-	}
-	print_rev_alphabet();
-	return res;
+map<int, string> LZW::create_reverse_alphabet() {
+	dictSize = 256;
+	map<int, string> dictionary;
+	for (int i = 0; i < 256; i++)
+		dictionary[i] = string(1, i);
+	return dictionary;
 }
 
--- main.cpp
+++ main.cpp
@@ -7,33 +7,23 @@
 
 #include "LZW.hpp"
 #include "OpenFile.hpp"
+#include <vector>
 
 using namespace std;
 
-
 int main(int argc, char* argv[]) {
-	cout << "enter main" << endl;
-	LZW* arch = new LZW(0,127);
-	cout << "done LZW" << endl;
+	vector<int> compressed;
+	LZW *arch = new LZW();
 	OpenFile* encode_file = new OpenFile("encode.txt", fstream::out);
-	cout << "begin opening" << endl;
-	OpenFile* war = new OpenFile("some.txt", fstream::in);
-	cout << "end opening" << endl;
-	string str = "abacabadabacabae";
-	cout << "begin encode" << endl;
-	encode_file->write(arch->encode(war->read_all()));
-	cout << "end encode" << endl;
+	OpenFile* war = new OpenFile("warpeace.txt", fstream::in);
+	string str = war->read_all();
+	arch->compress(str,back_inserter(compressed));
+	encode_file->write(compressed.begin(), compressed.end());
 	delete encode_file;
-	cout << "begin reading encoding" << endl;
 	OpenFile* file_for_decode = new OpenFile("encode.txt", fstream::in);
-	str = file_for_decode->read_all();
-	cout << "done reading encoding" << endl;
-	delete file_for_decode;
 	OpenFile* decode_file = new OpenFile("decode.txt", fstream::out);
-	cout << "begin decoding" << endl;
-	decode_file->write(arch->decode(str));
-	cout << "end decoding" << endl;
-	delete decode_file;
+	compressed = file_for_decode->read_all<vector<int>>();
+	decode_file->write(arch->decompress(compressed.begin(), compressed.end()));
 	return 0;
 }
 
