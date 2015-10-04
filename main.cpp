b10dd0c6d314fd27ea62bc6235727c812393dbd9
dimitriy <liman.dimitriy@gmail.com>
2015-10-04 14:49:53 +0300

    wrong working on str like "www"

--- main.cpp
+++ main.cpp
@@ -5,23 +5,34 @@
  *      Author: dimitriy
  */
 
-#include "LZW.h"
-#include "OpenFile.h"
+#include "LZW.hpp"
+#include "OpenFile.hpp"
 
 using namespace std;
 
 
 int main(int argc, char* argv[]) {
-	LZW* arch = new LZW('a','z');
+	cout << "enter main" << endl;
+	LZW* arch = new LZW(0,127);
+	cout << "done LZW" << endl;
 	OpenFile* encode_file = new OpenFile("encode.txt", fstream::out);
+	cout << "begin opening" << endl;
+	OpenFile* war = new OpenFile("some.txt", fstream::in);
+	cout << "end opening" << endl;
 	string str = "abacabadabacabae";
-	encode_file->write(arch->encode(str));
+	cout << "begin encode" << endl;
+	encode_file->write(arch->encode(war->read_all()));
+	cout << "end encode" << endl;
 	delete encode_file;
+	cout << "begin reading encoding" << endl;
 	OpenFile* file_for_decode = new OpenFile("encode.txt", fstream::in);
 	str = file_for_decode->read_all();
+	cout << "done reading encoding" << endl;
 	delete file_for_decode;
 	OpenFile* decode_file = new OpenFile("decode.txt", fstream::out);
+	cout << "begin decoding" << endl;
 	decode_file->write(arch->decode(str));
+	cout << "end decoding" << endl;
 	delete decode_file;
 	return 0;
 }
